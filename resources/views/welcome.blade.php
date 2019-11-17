0389da8b035552248eeb9895aec566fb8643a990
Amirul Mukminin <2019314445@isiswa.uitm.edu.my>
2019-11-17 14:13:39 +0800

    Change Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Minin Laravel's
                 </div>
 
                 <div class="links">
